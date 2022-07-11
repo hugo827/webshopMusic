750800982e4a417a9fe017527086ecf63fa5f0b3
hugo827 <81298292+hugo827@users.noreply.github.com>
2022-07-11 10:53:17 +0200

    Initial commit

---
+++ src/main/java/com/example/webshopmusic/WebshopMusicApplication.java
@@ -0,0 +1,13 @@
+package com.example.webshopmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebshopMusicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webshopmusic/WebshopMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webshopmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
